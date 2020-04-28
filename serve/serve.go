e8f9ab0fd74991b77d08d58a6ea3decf4295e1fe
arikui1911 <arikui.ruby@gmail.com>
2020-04-28 11:26:20 +0900

    refactor

--- serve/serve.go
+++ serve/serve.go
@@ -35,6 +35,51 @@ func init() {
 	memo.RegisterCommand(&info, 80)
 }
 
+type serveConfig struct {
+	memoDir          string
+	templateDirFile  string
+	templateBodyFile string
+}
+
+func run(c *cli.Context) error {
+	var cfg memo.Config
+	err := cfg.Load()
+	if err != nil {
+		return err
+	}
+
+	http.Handle("/", &serveConfig{
+		memoDir:          cfg.MemoDir,
+		templateDirFile:  cfg.TemplateDirFile,
+		templateBodyFile: cfg.TemplateBodyFile,
+	})
+	http.Handle("/assets/gfm/", http.StripPrefix("/assets/gfm", http.FileServer(gfmstyle.Assets)))
+	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir(cfg.AssetsDir))))
+
+	addr := c.String("addr")
+	var url string
+	if strings.HasPrefix(addr, ":") {
+		url = "http://localhost" + addr
+	} else {
+		url = "http://" + addr
+	}
+	browser.OpenURL(url)
+	return http.ListenAndServe(addr, nil)
+}
+
+func (c *serveConfig) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		c.handleList(w, r)
+		return
+	}
+	c.handleEntry(w, r)
+}
+
+type entry struct {
+	Name string
+	Body template.HTML
+}
+
 const templateDirContent = `
 <!DOCTYPE html>
 <html>
@@ -53,6 +98,44 @@ li {list-style-type: none;}
 </html>
 `
 
+func (c *serveConfig) handleList(w http.ResponseWriter, req *http.Request) {
+	f, err := os.Open(c.memoDir)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer f.Close()
+	files, err := f.Readdirnames(-1)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	files = memo.FilterMarkdown(files)
+	var entries []entry
+	for _, file := range files {
+		entries = append(entries, entry{
+			Name: file,
+			Body: template.HTML(runewidth.Truncate(memo.ReadFileFirstLine(filepath.Join(c.memoDir, file)), 80, "...")),
+		})
+	}
+	w.Header().Set("content-type", "text/html")
+	c.templateDirFile = memo.ExpandPath(c.templateDirFile)
+	var t *template.Template
+	if c.templateDirFile == "" {
+		t = template.Must(template.New("dir").Parse(templateDirContent))
+	} else {
+		t, err = template.ParseFiles(c.templateDirFile)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+	err = t.Execute(w, entries)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
 const templateBodyContent = `
 <!DOCTYPE html>
 <html>
@@ -66,96 +149,32 @@ const templateBodyContent = `
 </html>
 `
 
-type entry struct {
-	Name string
-	Body template.HTML
-}
-
-func run(c *cli.Context) error {
-	var cfg memo.Config
-	err := cfg.Load()
+func (c *serveConfig) handleEntry(w http.ResponseWriter, req *http.Request) {
+	b, err := ioutil.ReadFile(filepath.Join(c.memoDir, memo.EscapeMemoTitle(req.URL.Path)))
 	if err != nil {
-		return err
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		if req.URL.Path == "/" {
-			f, err := os.Open(cfg.MemoDir)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			defer f.Close()
-			files, err := f.Readdirnames(-1)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			files = memo.FilterMarkdown(files)
-			var entries []entry
-			for _, file := range files {
-				entries = append(entries, entry{
-					Name: file,
-					Body: template.HTML(runewidth.Truncate(memo.ReadFileFirstLine(filepath.Join(cfg.MemoDir, file)), 80, "...")),
-				})
-			}
-			w.Header().Set("content-type", "text/html")
-			cfg.TemplateDirFile = memo.ExpandPath(cfg.TemplateDirFile)
-			var t *template.Template
-			if cfg.TemplateDirFile == "" {
-				t = template.Must(template.New("dir").Parse(templateDirContent))
-			} else {
-				t, err = template.ParseFiles(cfg.TemplateDirFile)
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-					return
-				}
-			}
-			err = t.Execute(w, entries)
-			if err != nil {
-				log.Println(err)
-			}
-		} else {
-			p := filepath.Join(cfg.MemoDir, memo.EscapeMemoTitle(req.URL.Path))
-			b, err := ioutil.ReadFile(p)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			body := string(b)
-			if strings.HasPrefix(body, "---\n") {
-				if pos := strings.Index(body[4:], "---\n"); pos > 0 {
-					body = body[4+pos+4:]
-				}
-			}
-			body = string(github_flavored_markdown.Markdown([]byte(body)))
-			cfg.TemplateBodyFile = memo.ExpandPath(cfg.TemplateBodyFile)
-			var t *template.Template
-			if cfg.TemplateBodyFile == "" {
-				t = template.Must(template.New("body").Parse(templateBodyContent))
-			} else {
-				t, err = template.ParseFiles(cfg.TemplateBodyFile)
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-					return
-				}
-			}
-			t.Execute(w, entry{
-				Name: req.URL.Path,
-				Body: template.HTML(body),
-			})
+	body := string(b)
+	if strings.HasPrefix(body, "---\n") {
+		if pos := strings.Index(body[4:], "---\n"); pos > 0 {
+			body = body[4+pos+4:]
 		}
-	})
-	http.Handle("/assets/gfm/", http.StripPrefix("/assets/gfm", http.FileServer(gfmstyle.Assets)))
-	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir(cfg.AssetsDir))))
-
-	addr := c.String("addr")
-	var url string
-	if strings.HasPrefix(addr, ":") {
-		url = "http://localhost" + addr
-	} else {
-		url = "http://" + addr
 	}
-	browser.OpenURL(url)
-	return http.ListenAndServe(addr, nil)
+	body = string(github_flavored_markdown.Markdown([]byte(body)))
+	c.templateBodyFile = memo.ExpandPath(c.templateBodyFile)
+	var t *template.Template
+	if c.templateBodyFile == "" {
+		t = template.Must(template.New("body").Parse(templateBodyContent))
+	} else {
+		t, err = template.ParseFiles(c.templateBodyFile)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+	t.Execute(w, entry{
+		Name: req.URL.Path,
+		Body: template.HTML(body),
+	})
 }
